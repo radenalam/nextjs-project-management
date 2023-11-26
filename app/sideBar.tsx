3f56853216f48df15ca231d8eb277a0f3ce1b0d9
radenalam <radenalamsanjaya@gmail.com>
2023-11-26 15:05:32 +0700

    sidebar open close blm fix

--- app/sideBar.tsx
+++ app/sideBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import {
   FaDesktop,
   FaBookOpen,
@@ -11,30 +11,41 @@ import {
   FaUser,
   FaUsers,
 } from "react-icons/fa";
+import { FaArrowRightArrowLeft } from "react-icons/fa6";
 import classnames from "classnames";
 import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
   const currentPath = usePathname();
+  const [open, setOpen] = useState(true);
   console.log(currentPath);
 
   const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Project", href: "/projects" },
-    { label: "Task", href: "/tasks" },
-    { label: "Event", href: "/events" },
-    { label: "Client", href: "/clients" },
-    { label: "Team Member", href: "/team_members" },
+    { label: "Dashboard", href: "/", icon: <FaDesktop /> },
+    { label: "Project", href: "/projects", icon: <FaProjectDiagram /> },
+    { label: "Task", href: "/tasks", icon: <FaTasks /> },
+    { label: "Event", href: "/events", icon: <FaCalendarAlt /> },
+    { label: "Client", href: "/clients", icon: <FaUser /> },
+    { label: "Team Member", href: "/team_members", icon: <FaUsers /> },
   ];
   return (
-    <div className="flex flex-col w-64 h-screen px-4 py-8 bg-white border-r dark:bg-gray-800 dark:border-gray-600">
-      <div className="flex space-x-2 mb-2 items-center">
-        <Link href="/">
-          <FaBookOpen />
-          {/* <h1>Project</h1> */}
-        </Link>
-        <h1 className="text-xl">Project Managament</h1>
+    <div
+      className={`flex flex-col ${
+        open ? "w-72" : "w-23"
+      } h-screen px-4 py-8 bg-white border-r dark:bg-gray-800 dark:border-gray-600 `}
+    >
+      <div className="absolute cursor-pointer-right-5 top-9 w-7">
+        <FaArrowRightArrowLeft
+          onClick={() => {
+            setOpen(!open);
+          }}
+        />
       </div>
+      <Link className="flex mx-4 space-x-2 mb-2 items-center" href="/">
+        <FaBookOpen />
+        {open && <h1 className="text-xl">Project Managament</h1>}
+      </Link>
+
       <nav className="">
         <ul className="flex flex-col pt-3">
           {links.map((link) => (
@@ -42,15 +53,18 @@ const Sidebar = () => {
               style={{ marginBottom: "8px" }}
               key={link.href}
               className={classnames({
-                "text-zinc-200 bg-slate-400 px-2 py-1 rounded-sm":
+                "text-zinc-200 bg-slate-400 px-2 py-2 rounded-md":
                   link.href === currentPath, //To change the color of the current page
-                "text-zinc-500 px-2 py-1": link.href !== currentPath, //To change the color of the other pages
-                "hover:text-zinc-300 hover:bg-slate-600 hover:px-2 hover:py-1 hover:rounded-sm transition-colors":
+                "text-zinc-500 px-2 py-2": link.href !== currentPath, //To change the color of the other pages
+                "hover:text-zinc-300 hover:bg-slate-600 hover:px-2 hover:py-2 hover:rounded-md transition-colors":
                   true, //To change the color of the other pages when hovered
               })}
               href={link.href}
             >
-              {link.label}
+              <div className="flex items-center text-xl px-2 py-1">
+                {link.icon}
+                {open && <span className="ml-4">{link.label}</span>}
+              </div>
             </Link>
           ))}
         </ul>
